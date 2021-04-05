de529a7ea108f3a68c6dcf26d3d7c18c140ba027
ILFIR GALI <ilfir803@gmail.com>
2021-04-06 00:39:10 +0500

    calc

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,17 +1,13 @@
 let calculator = {
-
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
   sum() {
     return this.a + this.b;
   },
-
   mul() {
     return this.a * this.b;
-  },
-  read() {
-    this.a = +prompt('a?', 0);
-    this.b = +prompt('b?', 0);
   }
 };
-
-// НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
-window.calculator = calculator; // делает ваш калькулятор доступным глобально
+window.calculator = calculator;
