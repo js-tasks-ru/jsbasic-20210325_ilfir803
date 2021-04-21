2c06b3c75f65d53f68d54edf62a80bc062a7c8d1
ILFIR GALI <ilfir803@gmail.com>
2021-04-21 16:20:06 +0500

    задачи

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(/[\s,]+/);
+  arr = arr.map(function (item) {
+    return +item;
+  })
+  let outPut = arr.filter((item) => item !== 'NaN' && isFinite(item));
+  let max = Math.max.apply(null, outPut);
+  let min = Math.min.apply(null, outPut);
+
+  return { min, max };
 }
