a529fd3a0c3dc8518d62c133d44755af119312e2
ILFIR GALI <ilfir803@gmail.com>
2021-04-30 19:17:06 +0500

    slider

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,29 @@
 function initCarousel() {
-  // ваш код...
+  let rightBtn = document.querySelector('.carousel__arrow_right');
+  let leftBtn = document.querySelector('.carousel__arrow_left');
+  let carusel = document.querySelector('.carousel__inner');
+  let imgCount = document.querySelectorAll('.carousel__inner .carousel__slide').length
+  let itemWitdth = carusel.clientWidth;
+  let counter = 0;
+  // let imagesLength = imgAll.length;
+  // console.log(imagesLength) 
+
+  carusel.style.transform = 'translateX(' + `${-itemWitdth * counter}px)`;
+  leftBtn.style.display = 'none';
+
+  rightBtn.addEventListener('click', () => {
+    counter++;
+    if (counter > 0) leftBtn.style.display = '';
+    if (counter >= imgCount - 1) { rightBtn.style.display = 'none'; leftBtn.style.display = '' }
+        // else if (counter <= 2) { rightBtn.style.display = ''; leftBtn.style.display = 'none' };
+    carusel.style.transform = 'translateX(' + `${-itemWitdth * counter}px)`;
+  })
+
+  leftBtn.addEventListener('click', () => {
+    counter--;
+    if (counter <= imgCount - 1) rightBtn.style.display = '';
+    if (counter <= 0) { rightBtn.style.display = ''; leftBtn.style.display = 'none' }
+    // else if (counter <= 2) { rightBtn.style.display = ''; leftBtn.style.display = 'none' };
+    carusel.style.transform = 'translateX(' + `${-itemWitdth * counter}px)`;
+  })
 }
