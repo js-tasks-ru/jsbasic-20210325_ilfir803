5e590644485c806f2727cba0e8454918898ac19c
ILFIR GALI <ilfir803@gmail.com>
2021-11-24 14:49:23 +0500

    delete rows

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,34 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.data = rows;
+    this.row = '';
+  }
+  render() {
+    for (const { name, age, salary, city } of this.data) {
+      this.row += `<tr class="pane">
+      <td>${name}</td>
+      <td>${age}</td>
+      <td>${salary}</td>
+      <td>${city}</td>
+      <td><button class="remove-button">X</button></td>
+          </tr>
+      `;
+    }
+    return this.row;
+  }
+  get elem() {
+    const table = document.createElement('table');
+    table.insertAdjacentHTML('beforeEnd', this.render());
+    table.id = "container";
+    table.onclick = function (event) {
+      if (event.target.className != 'remove-button') return;
+      let pane = event.target.closest('.pane');
+      pane.remove();
+    };
+    return table;
   }
 }
+
+
+
+
